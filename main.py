92d0dade2e66c47fbc3dee504a0d57d60db2f925
Josrodjr <Josrodjr@gmail.com>
2020-02-17 00:31:58 -0600

    first parser

---
+++ main.py
@@ -0,0 +1,58 @@
+# define the rules of the program
+
+# separador = ' '
+
+operandos = {
+    ' ': 1,
+    '|': 1,
+    '*': 2,
+    '+': 2
+}
+
+test_clause2 = '(a|b)* palabra palabra palabra'
+test_clause = '(palabra (letra|letra)* a a b)* a'
+
+def parse_input(text):
+    # parsed input result
+    parsed = []
+    # parenthesis closure counters
+    parenthesis_cont = 0
+    # current token detected
+    token = ''
+    # for loop for recieved text
+    for char in range(len(text)):
+        if text[char] == '(' or text[char] == ')':
+            if parenthesis_cont >= 1:
+                token += text[char]
+        if text[char] == '(':
+            parenthesis_cont += 1
+        if text[char] == ')':
+            # TODO: send the current token found to same function in recursive motive
+            if parenthesis_cont == 1:
+                result = parse_input(token)
+                parsed.append(result)
+                token = ''
+            # TODO: empty found token
+            parenthesis_cont -= 1
+        # if text[char] == ' ':
+        #     # append to parsed the current value
+        #     if token != '':
+        #         parsed.append(token)
+        #     # empty found char
+        #     token = ''
+        if text[char] in operandos and parenthesis_cont == 0:
+            if token != '':
+                parsed.append(token)
+            token = ''
+            token += text[char]
+            parsed.append(token)
+            token = ''
+        # if none of the above do a concat to current found token and keep looking
+        elif text[char] != '(' and text[char] != ')':
+            token += text[char]
+        if char == len(text)-1:
+            parsed.append(token)
+    return parsed
+
+a = parse_input(test_clause)
+print(a)
